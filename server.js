971e9d7b8f494166fa48304dc04b493a02c3ec17
Beatriz Lopez <beatlm@gmail.com>
2020-07-16 23:53:58 +0200

    modified server.js

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
-const express = require('express');
-const http = require('http')
-const path = require('path');
-
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/index.html'));
+app.use(express.static(__dirname+'/dist/my-shopping-list'));
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/dist/my-shopping-list/index.html'));
 });
 
-const port = process.env.PORT || 3000;
-app.set('port', port);
-
-const server = http.createServer(app);
-server.listen(port, () => console.log('running'));
+app.listen(process.env.PORT || 8080);
